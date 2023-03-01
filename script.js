f2fc716170e6975a30b65d57b99a6554e10097ab
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-03-01 12:59:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //your code here
 let btn = document.getElementById('btn');
-let op = document.getElementById('respond');
-let Number = document.getElementById('Number');
+let output = document.getElementById('respond');
+let num = document.getElementById('num');
 function genRandomNum() {
     document.getElementById("num").innerHTML =Math.floor( Math.random()*40-20);
 }
 
 btn.addEventListener('click',function(){
     let input = document.getElementById('guess').value;
-    if (input == Number) {
-        op.innerHTML = 'Hot'
-    }else if(input<Number){
-        op.innerHTML = 'Cold'
+    if (input == num) {
+        output.innerHTML = 'Hot'
+    }else if(input<num){
+        output.innerHTML = 'Cold'
     }
-    if(input>Number){
-        op.innerHTML='Cold'
+    if(input>num){
+        output.innerHTML='Cold'
     }
 });
 
