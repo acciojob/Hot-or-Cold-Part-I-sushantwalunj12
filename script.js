cfe0a4e59d550a17b059489993ac47b916855761
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-03-01 10:33:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
-// const btn = document.querySelector('#btn');
-// const num = document.querySelector('#num');
-
-// btn.addEventListener('click', function() {
-//   const randomNum = Math.floor(Math.random() * 41) - 20;
-//   num.innerHTML = randomNum;
-// });
-
-let userInput = document.querySelector("input");
-let btn = document.querySelector("button");
-let output = document.querySelector("#num");
-
-function randomnumber() {
-    let range = 20 - (-20) + 1;  // max - min + 1
-
-    let random = Math.random() * range;
-
-    let result = Math.floor(random) + (-20);
-    console.log(userInput.value);
-    console.log(userInput.value - 5);
-    console.log(parseFloat(userInput.value) + parseFloat(5));
-    console.log(result);
-    if(((userInput.value - 5) <= result) && (result <= parseFloat(userInput.value) + parseFloat(5))){
-        return 'HOT';
-    }
-        return 'Cool';
+//your code here
+let btn = document.getElementById('btn');
+let op = document.getElementById('respond');
+let Number = document.getElementById('num');
+function genRandomNum() {
+    document.getElementById("num").innerHTML =Math.floor( Math.random()*40-20);
 }
 
-btn.addEventListener('click', () =>{
-    
-    output.innerHTML = randomnumber();
+btn.addEventListener('click',function(){
+    let input = document.getElementById('guess').value;
+    if (input == Number) {
+        op.innerHTML = 'Hot'
+    }else if(input<Number){
+        op.innerHTML = 'Cold'
+    }
+    if(input>Number){
+        op.innerHTML='Cold'
+    }
 });
+
