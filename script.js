5357d89edc1c72f0192a8f60073a844690769f64
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-03-01 20:25:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 //your code here
-let btn = document.getElementById('btn');
-let output = document.getElementById('respond');
-let num = document.getElementById('num');
-function genRandomNum() {
-    document.getElementById("num").innerHTML =Math.floor( Math.random()*40-20);
-}
+const response = document.querySelector("#respond");
+const random = Math.floor(Math.random() * (20 - 1 + 1) + 1);
 
-btn.addEventListener('click',function(){
-    let input = document.getElementById('guess').value;
-    if (input == num) {
-        output.innerHTML = 'Hot'
-    }else if(input<num){
-        output.innerHTML = 'Cold'
+let numOfGuess = 1;
+let prevDiff = 0;
+
+const checking = document.querySelector(".checking");
+
+
+checking.addEventListener("click", function (e) {
+  e.preventDefault();
+  const guessed = document.querySelector("#guess").value;
+  if (numOfGuess < 2) {
+    if (guessed < random) {
+      response.textContent = "guess higher";
+    } else if (guessed > random) {
+      response.textContent = "guess lower";
     }
-    if(input>num){
-        output.innerHTML='Cold'
+  } else {
+    let diff = guessed - random;
+    if (diff < prevDiff) {
+      response.textContent = "getting hotter";
+    } else if (guessed < random) {
+      response.textContent = "guess higher";
+    } else if (guessed > random) {
+      response.textContent = "guess lower";
     }
+  }
+  prevDiff = guessed - random;
+  numOfGuess++;
 });
-
